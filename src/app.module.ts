59d14b8bb138b60aea3f2fc089f6f49beb9bb954
hoshi <hoshi03918@gmail.com>
2021-03-06 11:17:59 +0900

    typeorm,nestjsを接続

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { ApiController } from './api/api.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config()
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER_NAME,
+      password: process.env.DB_USER_PASSWORD,
+      database: process.env.DATA_BASE,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, CatsController, ApiController],
   providers: [AppService],
 })
