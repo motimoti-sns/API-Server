cb705b4ef52fbc8c26aa2eec6b8e132538fdd8f5
hota1024 <hotalog@hota1024.com>
2021-03-15 20:27:08 +0900

    🐛 fix allowedHeaders

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
-    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+    allowedHeaders: '*',
   });
   await app.listen(5000);
 }
